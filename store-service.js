b97b23c6512d3b953741090f600c4939aea2e2a8
rkhan147 <rkhan147@myseneca.ca>
2024-07-31 11:41:16 -0400

    .

--- store-service.js
+++ store-service.js
@@ -1,3 +1,4 @@
+const pg = require('pg');
 const Sequelize = require('sequelize');
 var sequelize = new Sequelize('neondb', 'neondb_owner', 'XtM94YjSzFxU', {
     host: 'ep-withered-band-a56z79ty.us-east-2.aws.neon.tech',
